09075bf7202b17ce8de0af2a8c465c17b218cc8e
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-01-18 02:25:11 +0530

    changes in map

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -2,13 +2,22 @@ import { useEffect, useState } from "react"
 import map_layer from "../../assets/map/main_map_layer.png"
 
 export default function ITGMap() {
-    const [zoomIn, setZoomIn] = useState(10)
-    const [xPos, setXPos] = useState(500)
-    const [yPos, setYPos] = useState(500)
+    const [zoomIn, setZoomIn] = useState(1000)
+    const [xPos, setXPos] = useState(0)
+    const [yPos, setYPos] = useState(0)
+    const [location, setLocation] = useState([])
     const [grab, setGrab] = useState(false)
+    console.log(location)
     return (
-        <div className={grab ? "itg-map grabbing" : "itg-map"} onClick={()=>grab ? setGrab(false) : setGrab(true)} onMouseMove={(e)=>{grab ? [setXPos(e.clientX), setYPos(e.clientY)] : null}}>
-            <div className="layer" draggable={false} style={{backgroundImage: "url(" + map_layer + ")", backgroundPositionX: xPos + "px", backgroundPositionY: yPos + "px"}}></div>
+        <div className={grab ? "itg-map grabbing" : "itg-map"} onClick={(e)=>{setXPos(e.clientX); setYPos(e.clientY); setLocation(...location.push({xPos, yPos}))}} onDoubleClick={()=>zoomIn === 3000 ? setZoomIn(1000) : setZoomIn(zoomIn + 1000)}>
+            <img src={map_layer} className="layer" draggable={false} width={zoomIn} height={zoomIn}></img>
+            <div className="layer">
+                {
+                    location.map((ele, i)=>{
+                        return <div className="box" style={{top: ele.xPos + "px", left: ele.yPos + "px"}} title={"location " + i}>{ele.xPos + " " + ele.yPos}</div>
+                    })
+                }
+            </div>
         </div>
     )
 }
