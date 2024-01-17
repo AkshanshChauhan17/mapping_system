1c3a1567121482d371d74672c4e6611d00dd63ae
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-01-17 20:18:11 +0530

    changes in map

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -2,24 +2,13 @@ import { useState } from "react"
 import map_layer from "../../assets/map/main_map_layer.png"
 
 export default function ITGMap() {
-    const [zoomIn, setZoomIn] = useState(0)
+    const [zoomIn, setZoomIn] = useState(10)
     const [xPos, setXPos] = useState(0)
     const [yPos, setYPos] = useState(0)
+    const [grab, setGrab] = useState(false)
     return (
-        <div className="itg-map">
-            <img className="first-layer" style={{transform: "translateX(" + xPos + "%) translateY(" + yPos + "%) rotateZ(90deg) scaleX(" + zoomIn + "%) scaleY(" + zoomIn + "%)"}} src={map_layer} alt="layer-one"/>
-            <div className="controls">
-                <div className="zoom">
-                    <button onClick={()=>setZoomIn(zoomIn + 5)}>Zoom+</button>
-                    <button onClick={()=>setZoomIn(zoomIn - 5)}>Zoom-</button>
-                </div>
-                <div className="position">
-                    <button onClick={()=>setXPos(xPos + 1)}>Right</button>
-                    <button onClick={()=>setXPos(xPos - 1)}>Left</button>
-                    <button onClick={()=>setYPos(yPos + 1)}>Top</button>
-                    <button onClick={()=>setYPos(yPos - 1)}>Bottom</button>
-                </div>
-            </div>
+        <div className={grab ? "itg-map grabbing" : "itg-map"} draggable={false} onDrag={(e)=>{console.log(e.clientX, e.clientY)}} onDragStart={()=>setGrab(true)} onDragEnd={()=>setGrab(false)}>
+            <img className={grab ? "first-layer grabbing" : "first-layer"} draggable={false} loading="lazy" style={{transform: "translateX(" + xPos + "%) translateY(" + yPos + "%) rotateZ(90deg) scaleX(" + zoomIn + "%) scaleY(" + zoomIn + "%)"}} src={map_layer}/>
         </div>
     )
 }
