806197b7f4b9510518826c486516879b3c1e3eef
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-01-19 04:14:24 +0530

    new changes

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from "react"
 import map_layer from "../../assets/map/main_map_layer.png"
+import {FaRegBuilding, FaParking, FaVolleyballBall} from "react-icons/fa";
+import { GiShuttlecock } from "react-icons/gi"
+import pop_up_sound from "../../assets/sound/pop_up.mp3"
 
 export default function ITGMap({coords}){
+    const [hoverGroup, setHoverGroup] = useState();
+    const iconsList = [<FaRegBuilding color="white" />, <FaVolleyballBall color="white" />, <FaParking color="white" />, <GiShuttlecock color="white" />];
+    const audio = new Audio(pop_up_sound)
+    const play_pop_sound = ()=>{
+        audio.play()
+    }
     return (
         <div className="itg-map">
             <img className="layer-bg" src={map_layer} alt="itg_map_layer_default" />
@@ -9,9 +18,21 @@ export default function ITGMap({coords}){
                 {
                     coords.locations.map((e, i)=>{
                         return (
-                            <g className="location-group">
-                                <circle cx={e.x + "px"} cy={e.y + "px"} r="10px" fill="red" />
-                                <text x={e.x + 20 + "px"} y={e.y + 1 + "px"} dominant-baseline="middle" text-anchor="middle" font-size="16" fill="white" style={{fontWeight: 600}}>{coords.index_of}</text>
+                            <g className="location-group" key={i} onClick={()=>{hoverGroup===i ? setHoverGroup(-1) : setHoverGroup(i); play_pop_sound();}}>
+                                <circle cx={e.x + "px"} cy={e.y + "px"} r="12px" fill="rgb(200, 10, 10)" />
+                                <foreignObject x={e.x - 8 + "px"} y={e.y - 10 + "px"} width="20px" height="20px">
+                                    {iconsList[e.type]}
+                                </foreignObject>
+                                <rect x={e.x + 15 + "px"} y={e.y - 10 + "px"} width="175px" height="20px" fill="rgb(200, 10, 10)" rx="0" />
+                                <text x={e.x + 20 + "px"} y={e.y + 1 + "px"} dominant-baseline="middle" text-anchor="left" font-size="16" fill="white" style={{fontWeight: 600}}>{e.title}</text>
+                                <g className="location-group-details" style={hoverGroup===i ? {display: ""} : {display: "none"}}>
+                                    <rect x={e.x - 10 + "px"} y={e.y + 20 + "px"} width="200px" height="250px" fill="#FFFFFF" rx="2" />
+                                    <image href="https://news.ua.edu/wp-content/uploads/2018/09/hewsonbhall.jpg" x={e.x - 10 + "px"} y={e.y + "px"} width="200px" height="150px" rx="2" />
+                                    <foreignObject className="location-description" x={e.x - 5 + "px"} y={e.y + 140 + "px"} width="190" height="120">
+                                        <p style={{fontSize: 12, fontWeight: 500}}>{e.description}</p>
+                                        <a href="">view</a>
+                                    </foreignObject>
+                                </g>
                             </g>
                         )
                     })
--- src/components/navigation-menu/index.jsx
+++ src/components/navigation-menu/index.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react"
 import nav_setting from "./navigation_setting.json"
+import {FaRegBuilding, FaParking, FaVolleyballBall} from "react-icons/fa";
+import { GiShuttlecock } from "react-icons/gi";
 
 export default function NavigationMenu({set_coords}) {
     const [buttonActivate, setButtonActivate] = useState(0) 
+    const iconsList = [<FaRegBuilding />, <FaVolleyballBall col />, <FaParking col />, <GiShuttlecock col />];
     return (
         <div className="navigation-menu">
             <h1>{nav_setting.title}</h1>
@@ -16,7 +19,7 @@ export default function NavigationMenu({set_coords}) {
                                     element.content.map((inner_element, ii)=>{
                                         return(
                                             <button className={buttonActivate===i+""+ii ? "button-active" : "button-inactive"} key={ii} onClick={()=>{setButtonActivate(i + "" + ii); set_coords({index_of: buttonActivate, locations: inner_element.locations})}}>
-                                                {inner_element.value}
+                                                {iconsList[inner_element.locations[0].type]} {inner_element.value}
                                             </button>
                                         )
                                     })
