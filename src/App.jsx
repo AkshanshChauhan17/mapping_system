25bb5647daf11a7fbf6ba2d95d68aced476b4aeb
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-01-28 18:13:08 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 import ITGMap from './components/map'
-import NavigationMenu from './components/navigation-menu'
 
 function App() {
-  const [coords , setCoords] = useState({index_of: 0, locations: []})
   return (
     <div className='main'>
-      <NavigationMenu set_coords={setCoords} />
-      <ITGMap coords={coords} />
+      <ITGMap />
     </div>
   )
 }
--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,38 +1,36 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import map_layer from "../../assets/map/main_map_layer.png"
-import {FaRegBuilding, FaParking, FaVolleyballBall} from "react-icons/fa";
-import { GiShuttlecock } from "react-icons/gi"
-import pop_up_sound from "../../assets/sound/pop_up.mp3"
+import {FaLocationArrow} from "react-icons/fa"
 
-export default function ITGMap({coords}){
-    const [hoverGroup, setHoverGroup] = useState();
-    const iconsList = [<FaRegBuilding color="white" />, <FaVolleyballBall color="white" />, <FaParking color="white" />, <GiShuttlecock color="white" />];
-    const audio = new Audio(pop_up_sound)
-    const play_pop_sound = ()=>{
-        audio.play()
-    }
+export default function ITGMap(){
+    const [pins, setPins] = useState([])
+    const [takeinput, setTakeInput] = useState(false)
+    const [location, setLocation] = useState({x: 0, y: 0})
+    const [text, setText] = useState()
+    console.log(pins)
     return (
-        <div className="itg-map">
-            <img className="layer-bg" src={map_layer} alt="itg_map_layer_default" />
-            <svg className="first-layer" xmlns="http://www.w3.org/2000/svg" width="1000px" height="1000px">
+        <div className="main-map">
+            {
+                takeinput ?
+                <div className="take-input" style={{top: location.y + "px", left: location.x + "px"}}>
+                    <input type="text" placeholder="location name" onChange={(e)=>setText(e.target.value)} />
+                    <button onClick={(e)=>{setPins([...pins,{location, text: text}]); setTakeInput(false)}}>Add</button>
+                </div> : null
+            }
+            <img draggable={false} className="image-layer" src={map_layer} />
+            <svg className="svg-layer" onClick={(e)=>{setTakeInput(true); !takeinput ? null : setLocation({x: e.clientX + window.scrollX, y: e.clientY + window.scrollY})}}>
                 {
-                    coords.locations.map((e, i)=>{
+                    pins.map((e, i)=>{
                         return (
-                            <g className="location-group" key={i} onClick={()=>{hoverGroup===i ? setHoverGroup(-1) : setHoverGroup(i); play_pop_sound();}}>
-                                <circle cx={e.x + "px"} cy={e.y + "px"} r="12px" fill="rgb(200, 10, 10)" />
-                                <foreignObject x={e.x - 8 + "px"} y={e.y - 10 + "px"} width="20px" height="20px">
-                                    {iconsList[e.type]}
-                                </foreignObject>
-                                <rect x={e.x + 15 + "px"} y={e.y - 10 + "px"} width="175px" height="20px" fill="rgb(200, 10, 10)" rx="0" />
-                                <text x={e.x + 20 + "px"} y={e.y + 1 + "px"} dominant-baseline="middle" text-anchor="left" font-size="16" fill="white" style={{fontWeight: 600}}>{e.title}</text>
-                                <g className="location-group-details" style={hoverGroup===i ? {display: ""} : {display: "none"}}>
-                                    <rect x={e.x - 10 + "px"} y={e.y + 20 + "px"} width="200px" height="250px" fill="#FFFFFF" rx="2" />
-                                    <image href="https://news.ua.edu/wp-content/uploads/2018/09/hewsonbhall.jpg" x={e.x - 10 + "px"} y={e.y + "px"} width="200px" height="150px" rx="2" />
-                                    <foreignObject className="location-description" x={e.x - 5 + "px"} y={e.y + 140 + "px"} width="190" height="120">
-                                        <p style={{fontSize: 12, fontWeight: 500}}>{e.description}</p>
-                                        <a href="">view</a>
-                                    </foreignObject>
-                                </g>
+                            <g>
+                                <circle cx={e.location.x} cy={e.location.y} r={10} key={i} fill="red"></circle>
+                                {/* {
+                                    i!==0 ? 
+                                        <line x1={pins[i-1].x} y1={pins[i-1].y} x2={e.x} y2={e.y} stroke="white" strokeWidth={2}></line>
+                                    :
+                                        null
+                                } */}
+                                <text x={e.location.x} y={e.location.y} fill="white" fontWeight={700}>{e.text}</text>
                             </g>
                         )
                     })
